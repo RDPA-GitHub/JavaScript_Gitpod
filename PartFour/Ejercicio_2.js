766d7e2fbadb0c8e745dba84df8586a5085bb535
Ronald Pe#a <ronald.micros@gmail.com>
2023-02-20 13:12:26 -0300

    Gitpod JavaScript - Ejercicio 2 -> isOldEnoughToDrive

---
+++ PartFour/Ejercicio_2.js
@@ -0,0 +1,23 @@
+/* 
+  002 isOldEnoughToDrive
+
+    INSTRUCCIONES:
+  1. Escribe una funcion llamada isOldEnoughToDrive que dado un numero, en este caso una Edad, isOldEnoughToDrive
+     retorna si la persona de esa edad tiene la edad necesaria para conducir legalmente en los Estados Unidos.
+
+     EJEMPLO:
+      let output = isOldEnoughToDrive(22);
+      console.log(output);
+
+    PISTA:
+    -> La edad legal para poder conducir en los Estados Unidos es de 16 Años.
+*/
+//Code:
+
+function isOldEnoughToDrive(Age){
+  if(Age<16) return false;
+  else return true;
+}
+
+let output = isOldEnoughToDrive(22);
+console.log(output);
